147f7177f72be7bea762d6be2bda75d8b45c9e50
Alexjur5676 <alex9992142@mail.ru>
2022-08-10 15:13:22 +0700

    Add task13_alt2

---
+++ task13_alt2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int digitIndex = 3;
+if (number.Length > 0)
+{
+    if (number[0] == '-')
+    {
+        digitIndex++;
+    }
+}
+if (number.Length < digitIndex)
+{
+    Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+}
+else
+{
+    Console.WriteLine($"третья цифра {number[digitIndex - 1]} ");
+}
