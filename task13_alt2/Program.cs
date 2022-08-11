cf9acda0d9c6f2104fe2ff8a19844d1d865ee152
Alexjur5676 <alex9992142@mail.ru>
2022-08-11 14:12:01 +0700

    Add fix task13_2

--- task13_alt2/Program.cs
+++ task13_alt2/Program.cs
@@ -4,21 +4,54 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int digitIndex = 3;
+// if (number.Length > 0)
+// {
+//     if (number[0] == '-')
+//     {
+//         digitIndex++;
+//     }
+// }
+// if (number.Length < digitIndex)
+// {
+//     Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+// }
+// else
+// {
+//     Console.WriteLine($"третья цифра {number[digitIndex - 1]} ");
+// }
+
+
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int digitIndex = 2;
+// if (number.Length > 0)
+// if (number[0] == '-')
+// {
+//     digitIndex++;
+// }
+
+// if (number.Length <= digitIndex)
+// {
+//     Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+// }
+// else
+// {
+//     Console.WriteLine($"третья цифра {number[digitIndex]} ");
+// }
+
 Console.WriteLine("Введите число: ");
 string number = Console.ReadLine();
-int digitIndex = 3;
-if (number.Length > 0)
-{
-    if (number[0] == '-')
-    {
-        digitIndex++;
-    }
-}
-if (number.Length < digitIndex)
+int index = 2;
+if (number[0] == '-') index++;
+if (number.Length <= index)
 {
     Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
 }
 else
 {
-    Console.WriteLine($"третья цифра {number[digitIndex - 1]} ");
+    Console.WriteLine($"третья цифра {number[index]} ");
 }
