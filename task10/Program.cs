518fe491b23e538f6c81192602173a1e35cb14a8
Alexjur5676 <alex9992142@mail.ru>
2022-08-12 13:24:29 +0700

    Revert " Add gitignore"
    This reverts commit 6d0058f285591217337467efd963d039739d9e85.

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+//int number = new Random().Next(100, 1000);
+//Console.WriteLine($"Наше случайное число {number}");
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit1 = number / 100;
+int digit2 = number / 10;
+int result = digit2 - digit1*10;
+
+Console.WriteLine($"вторая цифра: {result}");
---
+++ task13_alt2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int digitIndex = 3;
+// if (number.Length > 0)
+// {
+//     if (number[0] == '-')
+//     {
+//         digitIndex++;
+//     }
+// }
+// if (number.Length < digitIndex)
+// {
+//     Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+// }
+// else
+// {
+//     Console.WriteLine($"третья цифра {number[digitIndex - 1]} ");
+// }
+
+
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int digitIndex = 2;
+// if (number.Length > 0)
+// if (number[0] == '-')
+// {
+//     digitIndex++;
+// }
+
+// if (number.Length <= digitIndex)
+// {
+//     Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+// }
+// else
+// {
+//     Console.WriteLine($"третья цифра {number[digitIndex]} ");
+// }
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int index = 2;
+if (number[0] == '-') index++;
+if (number.Length <= index)
+{
+    Console.WriteLine($"Во введенном числе {number} третья цифра отсутствует");
+}
+else
+{
+    Console.WriteLine($"третья цифра {number[index]} ");
+}
